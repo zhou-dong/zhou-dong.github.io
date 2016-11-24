f88e9af3166903dc8a4d38682ce05ba33d9700f8
zhou-dong <82224165@qq.com>
2016-11-24 16:33:24 -0600

    create about.html

--- views/pages/gulpfile.js
+++ views/pages/gulpfile.js
@@ -3,25 +3,14 @@ var gulp = require('gulp') ;
 var ejs = require('gulp-ejs') ;
 var rename = require('gulp-rename') ;
 
-gulp.task('ejs', function(){
+var domain = "https://zhou-dong.github.io/"
 
-	var head = {
-		title : 'about',
-		keywords : 'keywords',
-		description: 'description',
-		author : 'dong zhou'
-	}
-
-	var template = './about.ejs' ;
-
-
-	gulp.src(template)
-		.pipe(ejs({
-			title : 'about',
-			keywords : 'keywords',
-			description: 'description',
-			author : 'dong zhou'
-		}))
-		.pipe(rename('about.html'))
+gulp.task('about', function(){
+	var page = {url: domain + "/about/ahout.html", identifier: "identifier"} ;
+	var head = {title : 'ABOUT 周动', keywords : '读书体验、人生思考',
+		description: '周动的读书体验、人生经历、生活感想', author : '周动' };
+	gulp.src('./about.ejs')
+		.pipe(ejs({head: head, page: page}))
+		.pipe(rename('../../about/about.html'))
 		.pipe(gulp.dest('./'));
 }) ;
