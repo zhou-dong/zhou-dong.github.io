ff8d40b4eaf710754bafcd2c3d9252e1e1e90cd0
zhou-dong <82224165@qq.com>
2017-03-04 06:28:13 -0600

    update

--- about/gulpfile.js
+++ about/gulpfile.js
@@ -4,20 +4,20 @@ var rename = require('gulp-rename');
 var babel = require('gulp-babel');
 var browserify = require('browserify');
 
-
 var dest = './pages/';
 var views = './views/';
 
 gulp.task('intro', function() {
+	var content = {
+		'title': 'Prepared Interview'
+	};
 	gulp.src(views + 'intro.ejs')
-		.pipe(ejs())
+		.pipe(ejs(content))
 		.pipe(rename('intro.html'))
 		.pipe(gulp.dest(dest));
 });
 
 
 gulp.task('default', ['intro'], function() {
-
-	return gulp.watch("./views/*.*", "intro");
-
+	return gulp.watch("./views/*.*", ['intro']);
 });
