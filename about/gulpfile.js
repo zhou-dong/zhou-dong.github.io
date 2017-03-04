b6a1aa70ced137c1de042166a815b262d5754d55
zhou-dong <82224165@qq.com>
2017-03-04 05:49:25 -0600

    update

---
+++ about/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var ejs = require('gulp-ejs');
+var rename = require('gulp-rename');
+var babel = require('gulp-babel');
+var browserify = require('browserify');
+
+
+var dest = './dist/';
+var views = './views/';
+
+gulp.task('intro', function() {
+	gulp.src(views + 'intro.ejs')
+		.pipe(ejs())
+		.pipe(rename('intro.html'))
+		.pipe(gulp.dest(dest));
+});
+
+
+gulp.task('default', ['intro'], function() {
+
+	return gulp.watch("./views/*.*", "intro");
+
+});
